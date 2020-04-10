3981cefb27f46ee1ddd3ef0241a98c710c6f9fc9
zizilooper <zahrashadidzadeh@yahoo.com>
2020-04-11 00:24:15 +0430

    first App

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,80 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-container>
+      <v-row>
+        <v-col cols="12">
+          <v-progress-linear v-model="counter"></v-progress-linear>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col cols="4">
+          <v-text-field v-model="data"></v-text-field>
+          <v-btn id="btn" @click="addData" @submit.prevent dark large
+            >Click</v-btn
+          >
+        </v-col>
+      </v-row>
+      <v-row class="cards">
+      <div v-for="(item, index) in items" :key="index" >
+        <div @click="deleteItem(index)">
+          <v-card class="card">
+            <v-card-text>
+              <h4>{{ item }}</h4>
+            </v-card-text>
+          </v-card>
+        </div>
+      </div>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  name: "App",
+
+  data: () => ({
+    data: "",
+    items: [],
+    dele: "",
+    counter: "0",
+  }),
+  methods: {
+    addData() {
+      if (this.data !== "") {
+        this.items.push(this.data);
+        console.log(this.items);
+        this.data = "";
+        this.counter += 1;
+      }
+    },
+    deleteItem(index) {
+      this.items.splice(index, 1);
+      console.log(`delete`);
+      this.counter -= 1;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.card {
+  width: 200px;
+  height: 50px;
+  float: left;
+  margin: 10px;
+
+
+}
+.cards {
+  width: 100%;
+  background-color: rgb(230, 233, 238);
+  display: inline-block;
+  border-radius: 6px;
+  color: black;
+}
+#btn{
+  background-color: rgb(78, 96, 128);
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import Vuetify from 'vuetify'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  Vuetify,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
